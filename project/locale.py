64546c19ac234479f12b649b1b042b16f46422d3
Starwort <tcphone93@gmail.com>
2019-02-23 19:43:25 +0000

    Add locale

---
+++ project/locale.py
@@ -0,0 +1,8 @@
+class menu:
+    class unhelpful:
+        name = 'ウンヘルプフル メニュー'
+        nothing = 'ヅー ノシング'
+        save = 'サーヴ'
+    class fileselect:
+        file = ' 【ファイル】'
+        folder = ' 【フォルデル】'
