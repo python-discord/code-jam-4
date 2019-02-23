5e807ad363c0d6a69ea1d6de14ad39f92187bd70
johnny <john@techsystems.io>
2019-02-23 07:41:31 -0500

    layout does not work with kv file, but does function. Going to try programatically.

--- project/main.py
+++ project/main.py
@@ -1,14 +1,18 @@
 import kivy
 from kivy.app import App
-from kivy.uix.label import Label
-kivy.require('1.10.1')  # replace with your current kivy version !
+from kivy.uix.widget import Widget
+kivy.require('1.10.1')
 
 
-class MyApp(App):
+class AlphabeticHome(Widget):
+    pass
+
+
+class AlphabeticApp(App):
 
     def build(self):
-        return Label(text='Hello world')
+        return AlphabeticHome()
 
 
 if __name__ == '__main__':
-    MyApp().run()
+    AlphabeticApp().run()
