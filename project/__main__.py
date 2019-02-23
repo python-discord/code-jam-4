9f300dd4a7052fcac8bcde7f68fdd0f4d3d7f79d
Suhail <suhailm0608@gmail.com>
2019-02-23 19:10:57 +0000

    Subclassed asynctk.AsyncTk
    This does almost nothing at the moment, but will aid in organising the rest of the program
    
    Added a key bind

--- project/__main__.py
+++ project/__main__.py
@@ -3,107 +3,114 @@ import asyncio
 import os
 import pathlib
 
-root = tk.AsyncTk()
 
+class Framed(tk.AsyncTk):
 
-async def file_select():
-    """File select dialogue"""
-    manager = tk.AsyncToplevel(root)
-    dir = pathlib.Path()
-    dirbox = tk.AsyncEntry(manager)
-    dirbox.grid(row=0, column=0)
-    foldermap = tk.AsyncFrame(manager)
-    foldermap.grid(row=1, column=0)
+    def __init__(self):
+        super().__init__()
 
-    def populate_folder(folder):
-        nonlocal dir
-        dir = manager.dir
-        for i in ['..'] + os.listdir(folder):
-            if (dir / i).is_file():
+        self.setupMenu()
+        self.protocol("WM_DELETE_WINDOW", lambda: asyncio.ensure_future(self.save()))
+        self.bind("<Control-s>", lambda i: asyncio.ensure_future(self.destroy()))
+        self.bind("<Control-S>", lambda i: asyncio.ensure_future(self.destroy()))
 
-                async def cb(i=i):
-                    manager.file = dir / i
-                    await manager.destroy()
+    async def save(self):
+        print(await self.file_select())
 
-                tk.AsyncButton(
-                    foldermap,
-                    text=f"{i} [FILE]",
-                    callback=cb
-                ).pack()
-            elif (dir / i).is_dir():
+    def setupMenu(self):
+        self.menu = tk.AsyncMenu(self)
+        self.config(menu=self.menu)
 
-                async def cb(i=i):
-                    manager.dir = dir / i
-                    change_dir(manager.dir)
+        self.dropdown = tk.AsyncMenu(self.menu)
 
-                tk.AsyncButton(
-                    foldermap,
-                    text=f"{i} [FOLDER]",
-                    callback=cb
-                ).pack()
+        self.dropdown.add_command(label="Do nothing", command=lambda: None)
+        self.dropdown.add_command(
+            label="Save processor time",
+            command=lambda: asyncio.ensure_future(self.destroy())
+        )
 
-        async def new():
-            dialogue = tk.AsyncToplevel(manager)
-            filename = tk.AsyncEntry(dialogue)
-            filename.pack()
+        self.menu.add_cascade(label="Unhelpful menu", menu=self.dropdown)
 
-            async def cb():
-                if filename.get() != len(filename.get())*'.':
-                    for i in r'\/:*?"<>|':
-                        if i in filename.get():
-                            button.config(
-                                text='Save here\n[filename cannot contain any of:\\/:*?"<>|]'
-                            )
-                            break
-                    else:
-                        manager.file = manager.dir / filename.get()
-                        await dialogue.destroy()
-                        await manager.destroy()
-                else:
-                    button.config(text='Save here\n[filename cannot be empty or a special path]')
-            button = tk.AsyncButton(dialogue, text='Save here', callback=cb)
-            button.pack()
-            await manager.wait_window(dialogue)
-        tk.AsyncButton(foldermap, text='New file', callback=new).pack()
-
-    def boxcallback(*i):
-        change_dir(dirbox.get())
-
-    def change_dir(path):
-        nonlocal dir, foldermap
-        dir = pathlib.Path(path)
-        manager.dir = dir
-        asyncio.ensure_future(foldermap.destroy())
+    async def file_select(self):
+        """File select dialogue"""
+        manager = tk.AsyncToplevel(self)
+        dir = pathlib.Path()
+        dirbox = tk.AsyncEntry(manager)
+        dirbox.grid(row=0, column=0)
         foldermap = tk.AsyncFrame(manager)
         foldermap.grid(row=1, column=0)
-        populate_folder(dir)
 
-    dirbox.bind("<Return>", boxcallback)
-    change_dir(".")
-    await root.wait_window(manager)
-    return manager.file
+        def populate_folder(folder):
+            nonlocal dir
+            dir = manager.dir
+            for i in ['..'] + os.listdir(folder):
+                if (dir / i).is_file():
+
+                    async def cb(i=i):
+                        manager.file = dir / i
+                        await manager.destroy()
+
+                    tk.AsyncButton(
+                        foldermap,
+                        text=f"{i} [FILE]",
+                        callback=cb
+                    ).pack()
+                elif (dir / i).is_dir():
+
+                    async def cb(i=i):
+                        manager.dir = dir / i
+                        change_dir(manager.dir)
+
+                    tk.AsyncButton(
+                        foldermap,
+                        text=f"{i} [FOLDER]",
+                        callback=cb
+                    ).pack()
+
+            async def new():
+                dialogue = tk.AsyncToplevel(manager)
+                filename = tk.AsyncEntry(dialogue)
+                filename.pack()
+
+                async def cb():
+                    if filename.get() != len(filename.get())*'.':
+                        for i in r'\/:*?"<>|':
+                            if i in filename.get():
+                                button.config(
+                                    text='Save here\n[filename cannot contain any of:\\/:*?"<>|]'
+                                )
+                                break
+                        else:
+                            manager.file = manager.dir / filename.get()
+                            await dialogue.destroy()
+                            await manager.destroy()
+                    else:
+                        button.config(
+                            text='Save here\n[filename cannot be empty or a special path]'
+                        )
+                button = tk.AsyncButton(dialogue, text='Save here', callback=cb)
+                button.pack()
+                await manager.wait_window(dialogue)
+            tk.AsyncButton(foldermap, text='New file', callback=new).pack()
+
+        def boxcallback(*i):
+            change_dir(dirbox.get())
+
+        def change_dir(path):
+            nonlocal dir, foldermap
+            dir = pathlib.Path(path)
+            manager.dir = dir
+            asyncio.ensure_future(foldermap.destroy())
+            foldermap = tk.AsyncFrame(manager)
+            foldermap.grid(row=1, column=0)
+            populate_folder(dir)
+
+        dirbox.bind("<Return>", boxcallback)
+        change_dir(".")
+        await self.wait_window(manager)
+        return manager.file
 
 
-async def save():
-    print(await file_select())
-
-
-root.protocol("WM_DELETE_WINDOW", lambda: asyncio.ensure_future(save()))
-
-
-root.menu = tk.AsyncMenu(root)
-root.config(menu=root.menu)
-
-
-root.dropdown = tk.AsyncMenu(root.menu)
-
-root.dropdown.add_command(label="Do nothing", command=lambda: None)
-root.dropdown.add_command(
-    label="Save processor time",
-    command=lambda: asyncio.ensure_future(root.destroy())
-)
-
-root.menu.add_cascade(label="Unhelpful menu", menu=root.dropdown)
-
-
-root.mainloop()
+if __name__ == "__main__":
+    root = Framed()
+    root.mainloop()
