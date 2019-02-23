600d4311a0a44bc5e16e3330ad49ff4c9d09aaf0
0xf0f <0x0meta@gmail.com>
2019-02-23 21:35:29 +1300

    Updated EditorWindow and test_editor_window - Added a menu bar to EditorWindow. Added three menus: File, Edit and Help. Implemented rudimentary code for opening and saving files. 'Save As' is commented out for now and will be implemented later.
    - Changed EditorWindow to a tk.Toplevel object as this seemed more
    appropriate for it.
    
    - Switched to using grid instead of pack inside EditorWindow.
    
    - Added set_text method to EditorWindow. Other objects can use this to
    alter the text inside the editor's text box.
    
    - Changed test_editor_window.py to run root.mainloop() instead of
    editor_window.mainloop(). Also hid the root window, and added some code
    for closing the root window when the editor window closes.
    
    - Various clean ups to comments and code throughout to improve wording
    and confirmation to linting rules.

--- project/windows/test_editor_window.py
+++ project/windows/test_editor_window.py
@@ -1,9 +1,18 @@
-# this file will only be used to test the editor window and will be removed during release.
+# This file is used to test the editor window in isolation.
+# It will be removed during release.
 
 import tkinter as tk
 from project.windows.EditorWindow import EditorWindow
 
 if __name__ == '__main__':
     root = tk.Tk()
+
+    # Hide root window.
+    root.withdraw()
+
     editor_window = EditorWindow(root)
-    editor_window.mainloop()
+
+    # This will close the hidden root window when the editor window is closed.
+    editor_window.protocol('WM_DELETE_WINDOW', root.destroy)
+
+    root.mainloop()
