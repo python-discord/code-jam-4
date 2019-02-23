354e80932d115768bb7c67119b79e050380bdc4e
BWACpro <bobwithacamera@gmail.com>
2019-02-23 13:25:39 -0800

    Linted :P

--- project/Plugins/PredictiveText.py
+++ project/Plugins/PredictiveText.py
@@ -1,11 +1,10 @@
-#not done yet, feel free to change it :p
+# Not done yet, feel free to change it :p
+
 
 class PredictiveText():
 
     def _update_text(text):
         text = text.split()
         if 'hello' in text:
-            text[text.index('hello')] = 'help me' #chage this to what ever you want
+            text[text.index('hello')] = 'help me'  # Change this to what ever you want
         return text
-    
-print(PredictiveText._update_text(text = 'hello'))
