6b55f8f5cc7bee32f5751010b9da951215d7984b
0xf0f <0x0meta@gmail.com>
2019-02-23 17:34:24 +1300

    Added basic functionality to the EditorWindow class, which includes: - A tkinter.Text widget for entering text. - The method 'get_text' which other classes can use to access the contents of the text box in the editor window.
    Added code to test_editor_window.py for basic testing purposes.

--- project/windows/EditorWindow.py
+++ project/windows/EditorWindow.py
@@ -5,4 +5,17 @@ class EditorWindow(tk.Frame):
     """
     This class houses the main text editor window.
     """
-    pass
+
+    def __init__(self, master):
+        super().__init__(master)
+
+        # The main text entry in the window.
+        self.text_box = tk.Text(master)
+        self.text_box.pack()
+
+    def get_text(self) -> str:
+        """
+        A public method other objects can use to retrieve the text inside the editor window's text box.
+        :return: The text inside the editor window's text box.
+        """
+        return self.text_box.get("1.0", tk.END)
