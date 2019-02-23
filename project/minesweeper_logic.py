15c3961a76150ba4fda8ea4a40beb04112835c38
ravenkls <kristian.smith.2001@hotmail.co.uk>
2019-02-23 17:58:40 +0000

    put_mines_in_grid function complete

--- project/minesweeper_logic.py
+++ project/minesweeper_logic.py
@@ -1,3 +1,4 @@
+import random
 
 
 class Minesweeper:
@@ -9,6 +10,7 @@ class Minesweeper:
     def __init__(self, width, height):
         self.width = width
         self.height = height
+        self.grid = []
 
         # Initialise the game
         self.initialise_grid()
@@ -17,9 +19,15 @@ class Minesweeper:
         '''This refreshes self.grid variable into an array with all tiles being of value EMPTY'''
         self.grid = [[self.UNDISCOVERED for _ in range(self.width)] for _ in range(self.height)]
 
-    def put_bombs_in_grid(self, amount):
-        '''This puts X amount of bombs onto the grid in random places'''
-        pass
+    def put_mines_in_grid(self, amount) -> set:
+        '''This puts X amount of mines onto the grid in random places'''
+        coordinates = set()
+        while len(coordinates) < amount:
+            x, y = random.randrange(self.width), random.randrange(self.height)
+            coordinates.add((x, y))
+            self.grid[y][x] = self.MINE
+        return coordinates
+
 
     def click_tile(self, x, y) -> bool:
         '''This clicks a certain tile at point (x, y)
@@ -41,25 +49,29 @@ class Minesweeper:
             for tile in row:
                 print(print_characters[tile], end=' ')
             print()
+        print()
 
 
 if __name__ == '__main__':
     game = Minesweeper(16, 16)
     game.print_grid()
 
+    # Minesweeper Tests
+
     # Test for the bombs in grid function
-    game.put_bombs_in_grid(10)
+    game.put_mines_in_grid(10)
     assert [tile for tile_list in game.grid for tile in tile_list].count(game.MINE) == 10, \
         'There are not mines in the grid'
+    game.print_grid()
 
     # Test for the click_tile function to make tiles discoverable
     game_over = game.click_tile(5, 5)
     assert game.DISCOVERED in [tile for tile_list in game.grid for tile in tile_list], \
         'None of the tiles have been discovered after clicking'
+    game.print_grid()
 
     # Test the get_tile_number function returns an integer
     tile_number = game.get_tile_number(5, 5)
     assert isinstance(tile_number, int), \
         'The get_tile_number function didn\'t return an integer'
-
     game.print_grid()
