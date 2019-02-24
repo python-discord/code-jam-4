f1333dd2a124bafb7f3425acfd2415923024f61b
BWACpro <bobwithacamera@gmail.com>
2019-02-23 22:45:15 -0800

    add files :p

--- project/Plugins/PredictiveText.py
+++ project/Plugins/PredictiveText.py
@@ -1,10 +1,18 @@
 # Not done yet, feel free to change it :p
+from random import randint
 
 
 class PredictiveText():
 
-    def _update_text(text):
+    def _random_spelling_mistakes(text):
         text = text.split()
-        if 'hello' in text:
-            text[text.index('hello')] = 'help me'  # Change this to what ever you want
+        print(text)
+        for word in text:
+            length = len(word)
+            if length == 1:
+                return
+            spot = randint(0, length - 1)
+
         return text
+
+PredictiveText._random_spelling_mistakes('yelp hello I me')
