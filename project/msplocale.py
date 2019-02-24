db0a5ebec4ede264b72a5cfdb164eed0aa6d6115
Starwort <tcphone93@gmail.com>
2019-02-24 11:49:35 +0000

    Format locale

--- project/msplocale.py
+++ project/msplocale.py
@@ -1,18 +1,19 @@
 class menu:
     class unhelpful:
-        name = 'ウンヘルプフル メニュー'
-        nothing = 'ヅー ノシング'
-        save = 'サーヴ プロセソル タイム'
+        name = "ウンヘルプフル メニュー"
+        nothing = "ヅー ノシング"
+        save = "サーヴ プロセソル タイム"
 
     class fileselect:
-        file = ' 【ファイル】'
-        folder = ' 【フォルデル】'
+        file = " 【ファイル】"
+        folder = " 【フォルデル】"
 
         class button:
-            default = 'サヴ ヒル'
-            special = 'サヴ ヒル\n【ファイルナム カッノタ ビー エムプーチ オル ア スペシアル パス】'
+            default = "サヴ ヒル"
+            special = "サヴ ヒル\n【ファイルナム カッノタ ビー エムプーチ オル ア スペシアル パス】"
             invalid = 'サヴ ヒル\n【ファイルナム カッノタ コンタイヌ エニ オヴ: \\/:*?"<>|】'
-        new = 'ニュウ ファイル'
+
+        new = "ニュウ ファイル"
 
 
 class entrysection:
