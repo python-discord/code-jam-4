973abc11407935ef350ac6db07ccd71eb05417a8
MarkKoz <KozlovMark@gmail.com>
2019-02-23 20:04:13 -0800

    Implement adding media to the playlist

--- project/widgets/main_window.py
+++ project/widgets/main_window.py
@@ -1,5 +1,6 @@
-from PySide2.QtMultimedia import QMediaPlayer, QMediaPlaylist
-from PySide2.QtWidgets import QMainWindow
+from PySide2.QtCore import QUrl
+from PySide2.QtMultimedia import QMediaContent, QMediaPlayer, QMediaPlaylist
+from PySide2.QtWidgets import QFileDialog, QMainWindow
 
 from project.ui.main_window import Ui_MainWindow
 
@@ -15,3 +16,16 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.play_button.pressed.connect(self.player.play)
         self.previous_button.pressed.connect(self.playlist.previous)
         self.next_button.pressed.connect(self.playlist.next)
+
+        self.add_files_action.triggered.connect(self.add_media)
+
+    def add_media(self):
+        """Add media files selected from a file dialogue to the playlist."""
+        paths, _ = QFileDialog.getOpenFileNames(self, "Add files", "", "")
+
+        for path in paths:
+            print(path)
+            media = QMediaContent(QUrl.fromLocalFile(path))
+            self.playlist.addMedia(media)
+
+        # TODO: Update model
