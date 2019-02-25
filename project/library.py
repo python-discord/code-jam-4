8ce6ecc884642d675dd6beee2a8c7e94f316bbca
MarkKoz <KozlovMark@gmail.com>
2019-02-25 15:04:18 -0800

    Rewrite add_entry to access the db via Qt APIs
    * Rename add_entry to add_media
    * Add docstring to add_media

--- project/library.py
+++ project/library.py
@@ -1,6 +1,7 @@
 import sqlite3
+from typing import Any, Dict, Optional
 
-from PySide2.QtSql import QSqlDatabase
+from PySide2.QtSql import QSqlDatabase, QSqlQuery
 
 DB_NAME = "library.sqlite"
 
@@ -33,8 +34,34 @@ def create_db():
 
     # TODO: Handle possible errors if db fails to open
 
-def add_entry(tags):
-     with sqlite3.connect(DB_NAME) as conn:
-        cursor = conn.cursor()
-        cursor.execute( ''' INSERT INTO  LIBRARY VALUES (?,?,?,?,?,?,?,?)''', (6,tags['path'],tags['crc32'],tags.get('title',None),tags.get('ARTIST',None),tags.get('album',None),tags.get('DATE',None),tags.get('genre',None)))
-        return cursor.lastrowid
+def add_media(metadata: Dict[str, Any]) -> Optional[int]:
+    """Add media to the library database.
+
+    Parameters
+    ----------
+    metadata: Dict[str, Any]
+        The metadata of the media to add.
+
+    Returns
+    -------
+    Optional[int]
+        The ID of the added media. See :meth:`PySide2.QtSql.QSqlQuery.lastInsertId`.
+
+    """
+    query = QSqlQuery(QSqlDatabase.database())
+    query.prepare("""
+        insert into library (path, crc32, title, artist, album, date, genre)
+        values (?, ?, ?, ?, ?, ?, ?)
+    """)
+
+    query.addBindValue(metadata["path"])
+    query.addBindValue(metadata["crc32"])
+    query.addBindValue(metadata.get("title"))
+    query.addBindValue(metadata.get("artist"))
+    query.addBindValue(metadata.get("album"))
+    query.addBindValue(metadata.get("date"))
+    query.addBindValue(metadata.get("genre"))
+
+    query.exec_()
+
+    return query.lastInsertId()
