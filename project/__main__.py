eaf9b832d232b2bb4d2a276e5e526d1c19a5c7ee
JMS53 <juan@spinel.ch>
2019-02-25 18:46:31 +0100

    updated main.py

--- project/__main__.py
+++ project/__main__.py
@@ -7,7 +7,8 @@ documentation and linked resources here:
     https://docs.python.org/3/library/tkinter.html
 
 Requirements:
-    Python 3.
+    Python 3
+    tkinter
 """
 import tkinter as tk
 
@@ -18,12 +19,9 @@ class Calculator(tk.Frame):
         self.pack()
         
         self.entry = None       #Where the entered operations go
-        self.entry_label = None
+   
+        self.proc_label = None  #Where the processed operations are displayed
         
-        self.proc = None        #Displays processed operations
-        self.proc_label = None
-        
-        self.answer  = None     #Answer line
         self.answer_label = None
         
         self.a = tk.StringVar() #String representation of self.entry
@@ -39,69 +37,27 @@ class Calculator(tk.Frame):
         and buttons when called.
         """
 
-        self.pilot_label = tk.Label(
-            self,
-            text="Pilot: ",
-        )
-        self.pilot = tk.Entry(
+        self.entry = tk.Entry(
             self,
             width=30,
             textvariable = self.a,
         )
-        self.pilot_label.pack(side="top")
-        self.pilot.pack(side=tk.TOP)
-
-        self.pwd_label = tk.Label(
-            self,
-            text="Password: ",
-        )
-        self.pwd = tk.Entry(
-            self,
-            show="*",
-            width=30,
-            textvariable = self.b
-        )
-        self.pwd_label.pack(side="top")
-        self.pwd.pack(side=tk.TOP)         
+        self.entry.pack(side=tk.TOP)
         
-
-        self.launch_button = tk.Button(
+        self.proc_label = tk.Label(
             self,
-            text = self.launch_text.get(),
-			bg = "teal",
-			fg = "white",
-            command=self.do_countdown,
+            text ="",
         )
-        
-        self.launch_button.pack(side=tk.BOTTOM)
-
-    def do_countdown(self):
-        """
-        When the user clicks the login button, this callback
-        is invoked. Make it do a countdown. The first time
-        it is clicked, the button text should change to "3".
-        The next time to "2", then to "1", and then to "LIFTOFF!".
-
-        If the username or the password are blank, this
-        callback should not do anything.
-        """
-        
-        if (self.a.get() != "") and (self.b.get() != ""):
-            """
-            Checks both Pilot & PWD are not empty
-            """
-            self.counter += 1
-            if self.counter == 1:
-                self.launch_button["text"] = "3"
-            elif self.counter == 2:
-                self.launch_button["text"] = "2"
-            elif self.counter == 3:
-                self.launch_button["text"] = "1"
-            elif self.counter == 4:
-                self.launch_button["text"] = "LIFTOFF!"    
+        self.proc_label.pack(side = "top")
+                
+        self.ans_label = tk.Label(
+            self,
+            text = "",
+        )
+        self.ans_label.pack(side = "top")      
 
 
 root = tk.Tk()
-app = RocketShipControlPanel(master=root)
+app = Calculator(master=root)
 app.mainloop()
  
