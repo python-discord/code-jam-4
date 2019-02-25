7312f98dc3cabf285e95253cc8e940456029db3a
Doron <dorons900@gmail.com>
2019-02-25 20:33:50 +0200

    The program now annoys you!

--- project/frontend/weatherh8su.py
+++ project/frontend/weatherh8su.py
@@ -16,22 +16,30 @@ class weatherh8su:
         self.searchicon.grid(row=0,column=0)
         self.searchbar = Entry(master, width=70)
         self.searchbar.grid(row=0,column=1)
-        self.f = Frame(master, width=26,height=52,bg="red")
-        self.f.grid(column=2,row=0)
+        self.f = Frame(master, width=26, height=52)
+        self.f.grid(column=2,row=1)
         self.gear = Image.open("pics/gear.png")
         self.gear = ImageTk.PhotoImage(self.gear)
         self.settings = Label(master, image=self.gear)
         self.settings.bind("<Enter>", lambda a: self.annoy())
-        # self.settings.bind("<Leave>", lambda a: self.deannoy())
+        self.settings.bind("<Leave>", lambda a: self.deannoy())
+        self.f.bind("<Leave>", lambda a: self.deannoy())
         self.settings.grid(column=2,row=0)
 
     def annoy(self):
-        if self.settings.grid_info()["row"] == 0:
-            self.settings.grid(row=1)
+        self.settings.grid(row=1)
+        self.f.grid(row=0)
 
     def deannoy(self):
-        if self.settings.grid_info()["row"] == 1:
-            self.settings.grid(row=0)
+        if self.mouse_on(self.settings) or self.mouse_on(self.f):
+            return
+        self.settings.grid(row=0)
+        self.f.grid(row=1)
+
+    def mouse_on(self, widget):
+        if self.master.winfo_containing(*self.master.winfo_pointerxy()) == widget:
+            return True
+        return False
 
 root = Tk()
 root.geometry("600x600")
