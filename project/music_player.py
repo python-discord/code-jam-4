02d1001a08e09a818541aa0dfba85acf8880bd3a
MarkKoz <KozlovMark@gmail.com>
2019-02-25 04:04:10 -0800

    Create database
    An SQLite driver is being used. The database consists of a single table
    which has fields for metadata as well as the file's path and CRC32.

--- project/music_player.py
+++ project/music_player.py
@@ -2,12 +2,15 @@ import sys
 
 from PySide2.QtWidgets import QApplication
 
+from project import library
 from project.widgets.main_window import MainWindow
 
 
 def main():
     app = QApplication(sys.argv)
     app.setApplicationName("Music Player")
+
+    library.create_db()
 
     window = MainWindow()
     window.setWindowTitle("Music Player")
