3e89bca2b7ac7dc9c10cf3376fb3e6004a53abdb
CapnS <capnsurvivalist@gmail.com>
2019-02-26 14:07:04 -0600

    Adding Bio to cats

--- src/mainwindow.py
+++ src/mainwindow.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from PIL import Image, ImageTk
 import io
-from random import randint, choice
+from random import randint, choice, sample
 import asyncio
 import time
 import aiohttp
@@ -35,42 +35,45 @@ class Tinder:
         if not self.session:
             self.session = aiohttp.ClientSession()
         for _ in range(10):
+            cat_data = dict()
             if randint(0,9) == 9:
                 image_number = randint(1,10)
                 im = Image.open(os.path.join(self.dir, os.path.join("res", os.path.join("images", f"{image_number}.jpg"))))
                 im = im.resize((400, 400), Image.NEAREST)
                 image = ImageTk.PhotoImage(im)
-                async with self.session.get("https://www.pawclub.com.au/assets/js/namesTemp.json") as res:
-                    data = await res.json()
-                    names = choice(data["a"])
-                    name = names["name"]
-                self.images.append([image, True, name])
+                cat_data.update({"image" : image})
+                cat_data.update({"jumpscare": True})
             else:
+                cat_data.update({"jumpscare": False})
                 async with self.session.get('https://api.thecatapi.com/v1/images/search') as res:
                     data = await res.json()
                     url = data[0]['url']
                 async with self.session.get(url) as res:
                     image_bytes = await res.read()
                     im = Image.open(io.BytesIO(image_bytes))
-                    im = im.resize((400, 400), Image.NEAREST)
+                    im = im.resize((400, 450), Image.NEAREST)
                     image = ImageTk.PhotoImage(im)
-                async with self.session.get("https://www.pawclub.com.au/assets/js/namesTemp.json") as res:
-                    data = await res.json()
-                    letter = choice(list('acdefghijklmnopqrstuvwxyz'))
-                    names = choice(data[letter])
-                    name = names["name"]
-                self.images.append([image, False, name])
+                    cat_data.update({"image":image})
+            async with self.session.get("https://www.pawclub.com.au/assets/js/namesTemp.json") as res:
+                data = await res.json()
+                letter = choice(list('acdefghijklmnopqrstuvwxyz'))
+                cat = choice(data[letter])
+                cat_data.update({"name":cat["name"]})
+                cat_data.update({"gender":cat["gender"]})    
+            async with self.session.get("https://gist.githubusercontent.com/mbejda/453fdb77ef8d4d3b3a67/raw/e8334f09109dc212892406e25fdee03efdc23f56/hobbies.txt") as res:
+                text = await res.text()
+                all_hobbies = text.split("\n") 
+                hobby_list = sample(all_hobbies, 5)
+                list_of_hobbies = "\n •".join(hobby_list)
+                hobbies = f"Hobbies:\n •{list_of_hobbies}" 
+                cat_data.update({"hobbies":hobbies})
+            age = str(randint(1,15))
+            cat_data.update({"age":age})   
+            miles = randint(1,5)
+            location = f"{miles} miles away"
+            cat_data.update({"location":location})
+            self.images.append(cat_data)
         print(time.time()-t)
-
-
-    async def get_bio(self):
-        #code to get bio goes here
-        #probably want to return a dict of some sort
-        pass
-
-
-    def bio(self):
-        self.loop.run_until_complete(self.get_bio())
 
 
     def all_children(self):
@@ -81,36 +84,57 @@ class Tinder:
         return children
 
 
-    def new_image(self):
+    def new_image(self, cat= None):
         widget_list = self.all_children()
         for item in widget_list:
             item.pack_forget()
         self.frame = tk.Frame(self.root, bg = "black")
-        try:
-            image, jumpscare, cat_name = self.images.pop(0)
-        except IndexError:
-            self.loop.run_until_complete(self.get_cache())
-            image, jumpscare, cat_name = self.images.pop(0)
-        name = tk.Text(self.frame, width = 40, height = 1)
-        name.tag_configure("center", justify=tk.CENTER)
-        name.insert("1.0", cat_name)
-        name.tag_add("center", "1.0", tk.END)
-        name.configure(state="disabled")
-        name.pack(side = tk.TOP)
+        if not cat:
+            try:
+                cat = self.images.pop(0)
+            except IndexError:
+                self.loop.run_until_complete(self.get_cache())
+                cat = self.images.pop(0)
+        cat_name = cat["name"]
+        image = cat["image"]
+        jumpscare = cat["jumpscare"]
+        gender = cat["gender"].capitalize()
+        hobbies = cat["hobbies"]
+        age = cat["age"]
+        location = cat["location"]
+        if not jumpscare:
+            name = tk.Text(self.frame, width = 40, height = 1)
+            name.tag_configure("center", justify=tk.CENTER)
+            name.insert("1.0", cat_name)
+            name.tag_add("center", "1.0", tk.END)
+            name.configure(state="disabled")
+            name.pack(side = tk.TOP)
         tk.Label(self.frame, image=image).pack(side = tk.TOP)
-        tk.Button(self.frame, text="Like", background="green", command = self.new_image).pack(side = tk.RIGHT)
-        tk.Button(self.frame, text="Dislike", background="red", command = self.new_image).pack(side = tk.LEFT)
-        def get_bio():
-            widget_list = self.all_children()
-            for item in widget_list:
-                item.pack_forget()
-            self.frame = tk.Frame(self.root, bg = "black")
-            full_bio = self.loop.run_until_complete(self.get_bio())
-            bio = tk.Text(self.frame)
-        tk.Button(self.frame, text="Bio", background="blue", command = get_bio).pack(side = tk.BOTTOM)
         if jumpscare:
             mixer.music.load(os.path.join(self.dir, os.path.join("res", os.path.join("sounds", "jumpscare.mp3"))))
             mixer.music.play()
+            tk.Button(self.frame, text="Like", background="green", command = self.new_image).pack(side = tk.BOTTOM)
+        else:
+            tk.Button(self.frame, text="Like", background="green", command = self.new_image).pack(side = tk.RIGHT)
+            tk.Button(self.frame, text="Dislike", background="red", command = self.new_image).pack(side = tk.LEFT)
+            def back_to_photo():
+                self.new_image(cat)
+            def get_bio():
+                widget_list = self.all_children()
+                for item in widget_list:
+                    item.pack_forget()
+                self.frame = tk.Frame(self.root, bg = "black")
+                bio = tk.Text(self.frame)
+                bio.insert(tk.END, f"Name: {cat_name} \n")
+                bio.insert(tk.END, f"Age: {age} \n")
+                bio.insert(tk.END, f"Gender: {gender} \n")
+                bio.insert(tk.END, f"Location: {location} \n")
+                bio.insert(tk.END, f"{hobbies} \n")                
+                bio.configure(state="disabled")
+                bio.pack(side = tk.TOP)
+                tk.Button(self.frame, text="Back To Photo", background="blue", command = back_to_photo).pack(side = tk.BOTTOM)
+                self.frame.pack()
+            tk.Button(self.frame, text="Bio", background="blue", command = get_bio).pack(side = tk.BOTTOM)
         self.frame.pack()
         self.root.mainloop()
 
