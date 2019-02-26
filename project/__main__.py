19e6e45277d7a4e1f7c54c7e635da3de13f9a43f
JMS53 <juan@spinel.ch>
2019-02-26 18:17:21 +0100

    driving me crazy

--- project/__main__.py
+++ project/__main__.py
@@ -10,8 +10,8 @@ Requirements:
     Python 3
     tkinter
 """
-from tkinter import Button, Tk, StringVar
-
+from tkinter import Button, Tk, StringVar, Frame
+import tkinter as tk
 
 class Num(Button):
     def __init__(self, *args, **kwargs):
@@ -19,15 +19,8 @@ class Num(Button):
         self.val = int(self['text'])
         self['bg'] = "pink"
         self['fg'] = "purple"
-        
-    
-    def add_num(self):
-        """
-        Adds number to the expression awaiting to be evaluated. Since the app
-        does not like users, it will randomly assume the user pressed a 
-        neighbouring button.
-        """
-        print(self.val)
+        # command = add_num(self['text'])
+
 
 
 class Op(Button):
@@ -35,32 +28,67 @@ class Op(Button):
         Button.__init__(self, *args, **kwargs)
         self['bg'] = "purple"
         self['fg'] = "pink"
+        # command = add_num(self['text'])
+
+class Calculator(Frame):
+    def __init__(self, master = None):
+        super().__init__(master)
+        self.master = master
+        
+        # Number Buttons
+        self.btn1 = None
+        self.btn2 = None
+        self.btn3 = None
+        self.btn4 = None
+        self.btn5 = None
+        self.btn6 = None
+        self.btn7 = None
+        self.btn8 = None
+        self.btn9 = None
+        self.btn0 = None
+        
+        # Operator buttons
+        self.op1 = None
+        self.op2 = None
+        self.op3 = None
+        self.op4 = None
+        self.op5 = None
+        
+        self.create()
+    
+    def create(self):
+        """
+        Generates the actual GUI
+        """
+        self.btn1 = Num(self, text=("1"), command = self.add_num()).grid(row=1, column=0)
+        self.btn2 = Num(self, text=("2")).grid(row=1, column=1)
+        self.btn3 = Num(self, text=("3")).grid(row=1, column=1)
+        self.btn4 = Num(self, text=("4")).grid(row=1, column=1)
+        self.btn5 = Num(self, text=("5")).grid(row=1, column=1)
+        self.btn6 = Num(self, text=("6")).grid(row=1, column=1)
+        self.btn7 = Num(self, text=("7")).grid(row=1, column=1)
+        self.btn8 = Num(self, text=("8")).grid(row=1, column=1)
+        self.btn9 = Num(self, text=("9")).grid(row=1, column=1)
+        self.btn0 = Num(self, text=("0")).grid(row=1, column=1)
+        
+        # Operator buttons
+        self.op1 = Op(self, text=("+")).grid(row=1, column=3)
+        self.op2 = Op(self, text=("-")).grid(row=2, column=3)
+        self.op3 = Op(self, text=("*")).grid(row=3, column=3)
+        self.op4 = Op(self, text=("/")).grid(row=4, column=3)
+        self.op5 = Op(self, text=("=")).grid(row=4, column=2)
+        
+    def add_num(self):
+        print(self['val'])
 
 
-cal = Tk()
-cal.title("Calculator")
-operator = ""
-text_Input = StringVar()
-
-# Number Buttons
-btn1 = Num(cal, text=("1")).grid(row=1, column=0)
-btn2 = Num(cal, text=("2")).grid(row=1, column=1)
-btn3 = Num(cal, text=("3")).grid(row=1, column=2)
-btn4 = Num(cal, text=("4")).grid(row=2, column=0)
-btn5 = Num(cal, text=("5")).grid(row=2, column=1)
-btn6 = Num(cal, text=("6")).grid(row=2, column=2)
-btn7 = Num(cal, text=("7")).grid(row=3, column=0)
-btn8 = Num(cal, text=("8")).grid(row=3, column=1)
-btn9 = Num(cal, text=("9")).grid(row=3, column=2)
-btn0 = Num(cal, text=("0")).grid(row=4, column=1)
 
 
-# Operator buttons
-op1 = Op(cal, text=("+")).grid(row=1, column=3)
-op2 = Op(cal, text=("-")).grid(row=2, column=3)
-op3 = Op(cal, text=("*")).grid(row=3, column=3)
-op4 = Op(cal, text=("/")).grid(row=4, column=3)
-op5 = Op(cal, text=("=")).grid(row=4, column=2)
 
 
-cal.mainloop()
+
+
+
+root = tk.Tk()
+app = Calculator(master = root)
+app.mainloop()
