2eeed3bea8b38ea1ad77d39f003c9b6c87a10116
Runew0lf <runew0lf@gmail.com>
2019-02-26 12:25:25 +0000

    Make sure the user has scrolled to the bottom before we accept

--- project/ui/eula.py
+++ project/ui/eula.py
@@ -6,9 +6,12 @@ class Ui_EulaDialog(object):
         EulaDialog.setObjectName("EulaDialog")
         EulaDialog.resize(218, 701)
         self.clicked_button = None
+        self.scrolled_to_bottom = None
+        self.scrolled_to_top = None
         self.eula_TextEdit = QtWidgets.QPlainTextEdit(EulaDialog)
         self.eula_TextEdit.setGeometry(QtCore.QRect(10, 10, 201, 651))
         self.eula_TextEdit.setPlainText("")
+        self.eula_TextEdit.ensureCursorVisible()
         self.eula_TextEdit.setTextInteractionFlags(
             QtCore.Qt.TextSelectableByKeyboard | QtCore.Qt.TextSelectableByMouse)
         self.eula_TextEdit.setObjectName("eula_TextEdit")
@@ -19,13 +22,22 @@ class Ui_EulaDialog(object):
         self.eula_agree_button.setGeometry(QtCore.QRect(80, 670, 131, 23))
         self.eula_agree_button.setObjectName("eula_agree_button")
         self.eula_agree_button.clicked.connect(self.clickedButton)
+        self.eula_disagree_button.clicked.connect(self.clickedDisagree)
+
 
         self.retranslateUi(EulaDialog)
         QtCore.QMetaObject.connectSlotsByName(EulaDialog)
 
+    def clickedDisagree(self):
+        dlg = QtWidgets.QMessageBox(self)
+        dlg.setText("I believe you mean to select AGREE")
+        dlg.setIcon(QtWidgets.QMessageBox.Critical)
+        dlg.show()        
+
     def clickedButton(self):
         self.clicked_button = self.eula_agree_button
-        self.close()
+        if self.eula_TextEdit.verticalScrollBar().value() == self.eula_TextEdit.verticalScrollBar().maximum():
+            self.close()
 
     def retranslateUi(self, EulaDialog):
         _translate = QtCore.QCoreApplication.translate
