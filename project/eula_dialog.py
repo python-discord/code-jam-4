c0e88f2d8d66d5b30764e4b58e8e0936e760e836
Runew0lf <runew0lf@gmail.com>
2019-02-26 11:53:09 +0000

    Stopped EULA from being closed, added connections to agree button

--- project/eula_dialog.py
+++ project/eula_dialog.py
@@ -1,8 +1,14 @@
 from PyQt5.QtWidgets import QDialog
 from ui.eula import Ui_EulaDialog
+from PyQt5.QtCore import Qt
+
 
 class EulaDialog(QDialog, Ui_EulaDialog):
     def __init__(self, eula):
         super(EulaDialog, self).__init__()
         self.setupUi(self)
         self.eula_TextEdit.setPlainText(eula)
+        # Disable Help Menu, Close Button and System Menu
+        self.setWindowFlags(self.windowFlags() & ~Qt.WindowContextHelpButtonHint)
+        self.setWindowFlags(self.windowFlags() & ~Qt.WindowCloseButtonHint)
+        self.setWindowFlags(self.windowFlags() & ~Qt.WindowSystemMenuHint)
--- project/notepad.py
+++ project/notepad.py
@@ -38,7 +38,7 @@ class MainWindow(QMainWindow):
         centerPoint = QDesktopWidget().availableGeometry().center()
         qtRectangle.moveCenter(centerPoint)
         self.move(qtRectangle.topLeft())
-        self.setWindowIcon(QIcon('crocpad.ico'))
+        self.setWindowIcon(QIcon('project\crocpad.ico'))
 
         # Add Menus
         mainMenu = self.menuBar()
@@ -57,10 +57,13 @@ class MainWindow(QMainWindow):
         self.show()
 
         if app_config['License']['eulaaccepted'] != 'yes':
-            with open('EULA.txt', 'r', encoding="utf8") as f:
+            with open('project\EULA.txt', 'r', encoding="utf8") as f:
                 eula = f.read()
             self.eula_dialog = EulaDialog(eula)
-            self.eula_dialog.show()
+            self.eula_dialog.exec_()
+            if self.eula_dialog.clicked_button == self.eula_dialog.eula_agree_button:
+                # We click the agree button
+                pass
 
     def dialog_critical(self, alert_text):
         dlg = QMessageBox(self)
