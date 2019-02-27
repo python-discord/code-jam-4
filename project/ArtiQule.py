2f6d3c13fa83d1118ffa378bf18db607c5fe4499
Programnik <41002203+Programnik@users.noreply.github.com>
2019-02-27 20:36:10 +0100

    <NEW_FEATURES: -Fixed toolbar -added instances of PaintButton and put them in toolbar > <ERRORS: - App Crashes when clicking top area of application - color pallettes not displaying >

--- project/ArtiQule.py
+++ project/ArtiQule.py
@@ -18,6 +18,7 @@ def paintEvent(self, event):
         # do normal stuff ...
 """
 
+
 class PaintBoard(QMainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
@@ -63,14 +64,16 @@ class PaintBoard(QMainWindow):
                         tool.color = self.color
                     else:  # perhaps don't divide by 4
                         mixedColor = QColor(
-                            self.r - (max(self.r, tool.r//4) - min(self.r, tool.r//4)),
-                            self.g - (max(self.g, tool.g//4) - min(self.g, tool.g//4)),
-                            self.b - (max(self.b, tool.b//4) - min(self.b, tool.b//4)),
+                            self.r - (max(self.r, tool.r//4) -
+                                      min(self.r, tool.r//4)),
+                            self.g - (max(self.g, tool.g//4) -
+                                      min(self.g, tool.g//4)),
+                            self.b - (max(self.b, tool.b//4) -
+                                      min(self.b, tool.b//4)),
                             255
                             )
                         self.color, tool.color = mixedColor, mixedColor
                         tool.is_dipped = True
-
 
         class Tool:
             def __init__(self, toolName, brushSize,
@@ -88,7 +91,6 @@ class PaintBoard(QMainWindow):
                 self.set_icon()
                 PaintBoard.paintPattern = self.paintPattern
 
-
         class PointyPen(Tool):
             def __init__(self):
                 super().__init__("pointy_pen", 5)
@@ -100,12 +102,11 @@ class PaintBoard(QMainWindow):
                 while self.duration:
                     PaintBoard.draw = True  # error prone
 
-
         class ColorTool(Tool):
             def __init__(self, toolName, brushSize, paintPattern=None,
                          duration=randint(0, 30), r=0, g=0, b=0, t=0):
                 super().__init__(toolName, brushSize, paintPattern, duration)
-                self.r = r                           # TODO: less duration, more transprancy
+                self.r = r   # TODO: duration--, transparancy--
                 self.g = g
                 self.b = b
                 self.t = 0
@@ -127,7 +128,8 @@ class PaintBoard(QMainWindow):
         class StragglyPaintBrush(ColorTool):
             def __init__(self):
                 super().__init__("straggly_paintbrush", 30, "Straws")
-        #TODO: the two paintbrushes should drip self.color when not mousePressEvent
+        #TODO: the two paintbrushes should drip self.color
+        # when not mousePressEvent
 
         # like drawing with a rock hard paintbrush
         class SolidifiedBrush(ColorTool):
@@ -172,31 +174,33 @@ class PaintBoard(QMainWindow):
         save_file_action.triggered.connect(self.saveFile)
         exit_action.triggered.connect(self.exit)
 
-
-        pointy_pen_btn = QAction(QIcon('Design/icons/pointy_pen.png'), "Pointy Pen",
-                                 self)
+        pointy_pen_btn = QAction(QIcon('Design/icons/pointy_pen.png'),
+                                 "Pointy Pen", self)
         pointy_pen_btn.setShortcut("CTRL+P")
         pointy_pen_btn.setStatusTip("A very pointy pen")
         pointy_pen_btn.triggered.connect(pointy_pen.use)
 
-        fill_btn = QAction(QIcon('Design/icons/fill_empty.png'), "A bucket", self)
+        fill_btn = QAction(QIcon('Design/icons/fill_empty.png'),
+                           "A bucket", self)
         fill_btn.setShortcut("CTRL+P")
         fill_btn.setStatusTip("A bucket.")
         fill_btn.triggered.connect(fill.use)
 
-        straggly_paintbrush_btn = QAction(QIcon('Design/icons/straggly_paintbrush.png'),
+        straggly_paintbrush_btn = QAction(QIcon(
+                             'Design/icons/straggly_paintbrush.png'),
                                           "Straggly Paintbrush", self)
         straggly_paintbrush_btn.setShortcut("CTRL+A")
         straggly_paintbrush_btn.setStatusTip("A very Straggly Paintbrush.")
         straggly_paintbrush_btn.triggered.connect(straggly_paintbrush.use)
 
-        solidified_brush_btn = QAction( QIcon('Design/icons/solidified_brush.png'),
+        solidified_brush_btn = QAction(QIcon(
+                               'Design/icons/solidified_brush.png'),
                                         "A solid brush", self)
         solidified_brush_btn.setShortcut("CTRL+A")
         solidified_brush_btn.setStatusTip("Gorsh, that is a hard tip")
         solidified_brush_btn.triggered.connect(solidified_brush.use)
 
-        colors = c1, c2, c3, c4, c5, c6 = (PaletteButton()
+        colors = c1, c2, c3, c4, c5, c6 = (PaletteButton() \
                                             for pallette in range(6))
         self.toolbar = self.addToolBar("Toolbar")
         pallettes = p1, p2, p3, p4, p5, p6 = (QToolButton(self.toolbar)
@@ -208,12 +212,12 @@ class PaintBoard(QMainWindow):
         self.toolbar.addAction(solidified_brush_btn)
 
         for Color, pallette in zip(colors, pallettes):
-            pallette.setStyleSheet("QToolButton{background-color:rgb{}}".format(Color.color))
+            pallette.setStyleSheet("QToolButton{background-color:rgb{color}}"\
+                                   .format(color=Color.color))
             pallette.clicked.connect(Color.mix(self.currentTool))
             self.toolbar.addItem(pallette)
 
         self.show()
-
 
     def mousePressEvent(self, event):
         if event.button() == Qt.LeftButton:
