cb41c7835ec644ee82eb148e9382f7d3b4df1397
Patrick Yeadon <patrick-yeadon@users.noreply.github.com>
2019-02-28 17:25:19 -0500

    LetterGuesser Demo can guess your letter correctly!

--- project/AlphabetGuesser/letter_guesser.py
+++ project/AlphabetGuesser/letter_guesser.py
@@ -1,4 +1,5 @@
 import pickle
+import random
 
 
 class LetterGuesser:
@@ -10,11 +11,12 @@ class LetterGuesser:
     possible_characters
 
     === Methods ===
-    request_question: returns a question that can be printed to the user
-    remove_possible_letters: removes all characters from possible_characters that are contained in the given word/phrase
-    remove_possible_words: removes all words/phrases from dictionary that contain the exact same characters as the given
+    request_word: returns a random word from the dictionary
+    remove_possible_letters: removes all characters from possible_characters that are not contained in the given
+                             word/phrase
+    remove_possible_words: removes all words/phrases from dictionary that contain none of the characters in the given
                            word/phrase
-    contains_all_letters: Returns True only if all the letters in word_2 are contained in word_1
+    contains_none_letters: Returns True only if none the letters in word_2 are contained in word_1
     """
     def __init__(self):
         self.dictionary = None
@@ -25,31 +27,44 @@ class LetterGuesser:
             self.dictionary = pickle.load(openfile)
             openfile.close()
 
-        # Test word, this assumes that the user said that this word doesn't contain any of the letters they want
-        self.remove_possible_words('qweoiuirwpeoitulkjashdflghmnbzxcv')
-
-    def request_question(self) -> str:
-        pass
+    def request_word(self) -> str:
+        return random.choice(list(self.dictionary))
 
     def remove_possible_letters(self, word) -> None:
-        pass
+        characters_to_delete = []
+        for character in self.possible_characters:
+            if character not in word:
+                characters_to_delete.append(character)
+        for character in characters_to_delete:
+            self.possible_characters.remove(character)
 
     def remove_possible_words(self, word) -> None:
         words_to_delete = []
         for elem in self.dictionary:
-            if self.contains_all_letters(word, elem):
-                print('{0} contains all the letters in {1}'.format(word, elem))
+            if self.contains_none_letters(elem, word):
                 words_to_delete.append(elem)
         for elem in words_to_delete:
             del self.dictionary[elem]
 
     @staticmethod
-    def contains_all_letters(word_1, word_2) -> bool:
-        for letter in word_2:
-            if letter not in word_1:
+    def contains_none_letters(word_1, word_2) -> bool:
+        for letter in word_1:
+            if letter in word_2:
                 return False
         return True
 
 
 if __name__ == '__main__':
     guesser = LetterGuesser()
+    while True:
+        word = guesser.request_word()
+        yes_no = input('Is your letter in the word/phrase:\n{}\n(Yes/No):'.format(word))
+        if yes_no == 'Yes':
+            guesser.remove_possible_words(word)
+            guesser.remove_possible_letters(word)
+        if len(guesser.possible_characters) == 1:
+            print('The letter you want is \'{0}\''.format(guesser.possible_characters[0]))
+            break
+        if len(guesser.possible_characters) < 1:
+            print('A mistake has been made; try again')
+            break
