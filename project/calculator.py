fec0f6f81a031009249c81a253364d7a2eced342
JMS53 <juan@spinel.ch>
2019-02-28 22:10:55 +0100

    random button placement

--- project/calculator.py
+++ project/calculator.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import random
 
 def btnClick(numbers) :
     global operator
@@ -14,7 +15,18 @@ def btnEqualsInput():
     global operator
     sumup=str(eval(operator))
     text_Input.set(sumup)
-    operator=""
+    #operator=""
+
+numbers = [x for x in range(10)]
+
+
+
+def assign_num():
+    index = random.randint(0, len(numbers)-1)
+    num = numbers[index]
+    if len(numbers) >= 2:
+        numbers.pop(index)
+    return num    
 
 cal = Tk()
 cal.title("Calculator")
@@ -26,16 +38,28 @@ def add_num(val):
 
 txtDisplay = Entry(cal,font=('arial',20,'bold'), textvariable=text_Input, bd=30, insertwidth=4, bg="pink", justify='right').grid(columnspan=4)
 
-btn1=Button(cal,  text=("1"), padx=16, bd=8, font=('arial',20,'bold'),  fg="purple", bg="pink", command=lambda:btnClick(1)).grid(row=1,column=0)
-btn2=Button(cal,  text=("2"), padx=16, bd=8, font=('arial',20,'bold'),  fg="purple", bg="pink", command=lambda:btnClick(2)).grid(row=1,column=1)
-btn3=Button(cal,  text=("3"), padx=16, bd=8, font=('arial',20,'bold'),  fg="purple", bg="pink", command=lambda:btnClick(3)).grid(row=1,column=2)
-btn4=Button(cal,  text=("4"), padx=16, bd=8, font=('arial',20,'bold'),  fg="purple", bg="pink", command=lambda:btnClick(4)).grid(row=2,column=0) 
-btn5=Button(cal,  text=("5"), padx=16, bd=8, font=('arial',20,'bold'),  fg="purple", bg="pink", command=lambda:btnClick(5)).grid(row=2,column=1) 
-btn6=Button(cal,  text=("6"), padx=16, bd=8, font=('arial',20,'bold'),  fg="purple", bg="pink", command=lambda:btnClick(6)).grid(row=2,column=2)
-btn7=Button(cal,  text=("7"), padx=16, bd=8, font=('arial',20,'bold'),  fg="purple", bg="pink", command=lambda:btnClick(7)).grid(row=3,column=0)
-btn8=Button(cal,  text=("8"), padx=16, bd=8, font=('arial',20,'bold'),  fg="purple", bg="pink", command=lambda:btnClick(8)).grid(row=3,column=1) 
-btn9=Button(cal,  text=("9"), padx=16, bd=8, font=('arial',20,'bold'),  fg="purple", bg="pink", command=lambda:btnClick(9)).grid(row=3,column=2)
-btn0=Button(cal,  text=("0"), padx=16, bd=8, font=('arial',20,'bold'),  fg="purple", bg="pink", command=lambda:btnClick(0)).grid(row=4,column=1)
+a = assign_num()
+b = assign_num()
+c = assign_num()
+d = assign_num()
+e = assign_num()
+f = assign_num()
+g = assign_num()
+h = assign_num()
+i = assign_num()
+j = assign_num()
+
+
+btn1=Button(cal,  text=(str(a)), padx=16, bd=8, font=('arial',20,'bold'),  fg="purple", bg="pink", command=lambda:btnClick(a)).grid(row=1,column=0)
+btn2=Button(cal,  text=(str(b)), padx=16, bd=8, font=('arial',20,'bold'),  fg="purple", bg="pink", command=lambda:btnClick(b)).grid(row=1,column=1)
+btn3=Button(cal,  text=(str(c)), padx=16, bd=8, font=('arial',20,'bold'),  fg="purple", bg="pink", command=lambda:btnClick(c)).grid(row=1,column=2)
+btn4=Button(cal,  text=(str(d)), padx=16, bd=8, font=('arial',20,'bold'),  fg="purple", bg="pink", command=lambda:btnClick(d)).grid(row=2,column=0) 
+btn5=Button(cal,  text=(str(e)), padx=16, bd=8, font=('arial',20,'bold'),  fg="purple", bg="pink", command=lambda:btnClick(e)).grid(row=2,column=1) 
+btn6=Button(cal,  text=(str(f)), padx=16, bd=8, font=('arial',20,'bold'),  fg="purple", bg="pink", command=lambda:btnClick(f)).grid(row=2,column=2)
+btn7=Button(cal,  text=(str(g)), padx=16, bd=8, font=('arial',20,'bold'),  fg="purple", bg="pink", command=lambda:btnClick(g)).grid(row=3,column=0)
+btn8=Button(cal,  text=(str(h)), padx=16, bd=8, font=('arial',20,'bold'),  fg="purple", bg="pink", command=lambda:btnClick(h)).grid(row=3,column=1) 
+btn9=Button(cal,  text=(str(i)), padx=16, bd=8, font=('arial',20,'bold'),  fg="purple", bg="pink", command=lambda:btnClick(i)).grid(row=3,column=2)
+btn0=Button(cal,  text=(str(j)), padx=16, bd=8, font=('arial',20,'bold'),  fg="purple", bg="pink", command=lambda:btnClick(j)).grid(row=4,column=1)
 
 btncle=Button(cal,  text=("C"), padx=16, bd=8, font=('arial',20,'bold'),  fg="purple", bg="pink", command=btnClearDisplay).grid(row=4,column=0)
 
@@ -45,7 +69,7 @@ btnbra=Button(cal,  text=("("), padx=16, bd=8, font=('arial',20,'bold'),  fg="pu
 btnket=Button(cal,  text=(")"), padx=16, bd=8, font=('arial',20,'bold'),  fg="purple", bg="pink", command=lambda:btnClick(")")).grid(row=5,column=1)
 btndec=Button(cal,  text=("."), padx=16, bd=8, font=('arial',20,'bold'),  fg="purple", bg="pink", command=lambda:btnClick(".")).grid(row=5,column=2)
 
-btnpi = Button(cal, text="pie"),  padx=16, bd=8, font=('arial',20,'bold'),  fg="purple", bg="pink", command=lambda:btnClick("3")).grid(row=4,column=2) 
+btndpi=Button(cal,  text=("pie"), padx=16, bd=8, font=('arial',20,'bold'),  fg="purple", bg="pink", command=lambda:btnClick("3")).grid(row=4,column=2)
 
 # space here for one more button maybe we should add pi you know like in case the user gets a bit hungry, this app may ultimately hate the users but we have to lure them in at the start with nice things maybe a nice cherry pi will do just fine
 
