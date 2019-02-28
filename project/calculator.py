843700e990a80c5aad2a13375ebe938950423c0a
JMS53 <juan@spinel.ch>
2019-02-28 13:50:40 +0100

    added pi

--- project/calculator.py
+++ project/calculator.py
@@ -1,4 +1,4 @@
-from tkinter import*
+from tkinter import *
 
 def btnClick(numbers) :
     global operator
@@ -39,12 +39,15 @@ btn0=Button(cal,  text=("0"), padx=16, bd=8, font=('arial',20,'bold'),  fg="purp
 
 btncle=Button(cal,  text=("C"), padx=16, bd=8, font=('arial',20,'bold'),  fg="purple", bg="pink", command=btnClearDisplay).grid(row=4,column=0)
 
-btnequ=Button(cal,  text=("="), padx=16, bd=8, font=('arial',20,'bold'),  fg="purple", bg="pink", command=btnEqualsInput).grid(row=4,column=2)
+btnequ=Button(cal,  text=("="), padx=16, bd=8, font=('arial',20,'bold'),  fg="purple", bg="pink", command=btnEqualsInput).grid(row=5,column=3)
 
 btnbra=Button(cal,  text=("("), padx=16, bd=8, font=('arial',20,'bold'),  fg="purple", bg="pink", command=lambda:btnClick("(")).grid(row=5,column=0)
 btnket=Button(cal,  text=(")"), padx=16, bd=8, font=('arial',20,'bold'),  fg="purple", bg="pink", command=lambda:btnClick(")")).grid(row=5,column=1)
 btndec=Button(cal,  text=("."), padx=16, bd=8, font=('arial',20,'bold'),  fg="purple", bg="pink", command=lambda:btnClick(".")).grid(row=5,column=2)
-#space here for one more button maybe we should add pi you know like in case the user gets a bit hungry, this app may ultimately hate the users but we have to lure them in at the start with nice things maybe a nice cherry pi will do just fine
+
+btnpi = Button(cal, text="pie"),  padx=16, bd=8, font=('arial',20,'bold'),  fg="purple", bg="pink", command=lambda:btnClick("3")).grid(row=4,column=2) 
+
+# space here for one more button maybe we should add pi you know like in case the user gets a bit hungry, this app may ultimately hate the users but we have to lure them in at the start with nice things maybe a nice cherry pi will do just fine
 
 btnadd=Button(cal,  text=("+"), padx=16, bd=8, font=('arial',20,'bold'),  fg="purple", bg="pink", command=lambda:btnClick("+")).grid(row=1,column=3)
 btnsub=Button(cal,  text=("-"), padx=16, bd=8, font=('arial',20,'bold'),  fg="purple", bg="pink", command=lambda:btnClick("-")).grid(row=2,column=3)
