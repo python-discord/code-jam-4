dc780e64b10f8849dfa1bbec4e765d7f84e6e86f
0xf0f <0x0meta@gmail.com>
2019-03-01 16:22:24 +1300

    Created CloppyWindow and a file for testing it.
    - CloppyWindow is intended to be used as a dialog for asking the user for some input. Currently there are two subclasses of it: CloppyButtonWindow and CloppyInputWindow - One for multi-choice questions with buttons, the other for direct text input from the user.
    
    - Each CloppyWindow has a choice_made event to which callbacks can be assigned. It communicates the choice made with the callbacks by sending them a CloppyWindowChoiceMadeEventData object.
    
    - Added some constants for Cloppy's image and greeting.
    
    - Added settings window stub file.
    
    - Move around a bit of code in editor_window.py to make it cleaner, plus a comment to indicate that '\x08' is the character code for backspace.

---
+++ project/windows/cloppy_window_events.py
@@ -0,0 +1,14 @@
+from project.functionality.events import Event
+
+
+class CloppyChoiceMadeEvent(Event):
+    def __call__(self, message, choice):
+        super().__call__(
+            CloppyChoiceMadeEventData(message, choice)
+        )
+        
+
+class CloppyChoiceMadeEventData:
+    def __init__(self, message, choice):
+        self.message = message
+        self.choice = choice
