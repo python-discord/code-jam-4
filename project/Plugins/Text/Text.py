6130154a04453c112fa72e3ad256660cd5a415b6
BWACpro <40532058+BWACpro@users.noreply.github.com>
2019-03-02 07:19:10 +1100

    synonym updates, the parse.py file was uneeded

--- project/Plugins/Text/Text.py
+++ project/Plugins/Text/Text.py
@@ -2,17 +2,23 @@ import string
 from random import randint, choice
 import requests
 from lxml import html
-from thesaurus import Word
+import thesaurus
+import time
+
 
 def apply(text):
     """Use this function, not any other one. It as a 50/50 chance of applying each"""
 
-    if randint(0, 1):
-        text = synonym(text)
-        return text
-    else:
-        text = quotify(text)
-        return text
+    text = synonym(text)
+    return text
+
+    # if randint(0, 1):
+    #     text = synonym(text)
+    #     return text
+    # else:
+    #     text = quotify(text)
+    #     return text
+
 
 def random_spelling_mistakes(text):
     text = text.split()
@@ -46,18 +52,32 @@ def quotify(text):
 
 
 def synonym(text):
+    start = time.time()
+
+    #if len(text) >
     print('Processing...')
     text = text.split()
     new_words = ''
     for word in text:
         print("Processing Word '", word, "'")
         try:
-            w = Word(word)
+            w = thesaurus.Word(word)
             w.synonyms('all')
             text = w.synonyms()[randint(0, len(w))]
             final = ''.join(text)
             new_words = new_words + ' ' + final
-        except:
-            print('Error. Skipping word:', word)
+        except thesaurus.exceptions.MisspellingError:
+            print("Error. Skipping word: ' ", word, " '")
             new_words = new_words + ' ' + word
+            new_words = new_words + ' ' + word
+        except thesaurus.exceptions.WordNotFoundError:
+            print("Error. Skipping word: ' ", word, " '")
+            new_words = new_words + ' ' + word
+            new_words = new_words + ' ' + word
+        except TypeError:
+            print("Error. Skipping word: ' ", word, " '")
+            new_words = new_words + ' ' + word
+            new_words = new_words + ' ' + word
+    end = time.time()
+    print(end - start)
     return new_words
