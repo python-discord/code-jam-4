55a5a4a1d274120ffa3e959e9e65fd1581ab0374
kosayoda <kieransiek@gmail.com>
2019-03-01 09:58:44 +0800

    Modified the main ui window to update the table upon initialization

--- project/ui/ui.py
+++ project/ui/ui.py
@@ -3,6 +3,7 @@ from PySide2.QtWidgets import QDesktopWidget, QMainWindow, qApp
 from .ui_files.ui_main import Ui_MainApplication
 from .add_task import AddTask
 from .datacomm import DataComm
+from .table_model import TableModel
 
 
 class MainApplication(QMainWindow, Ui_MainApplication):
@@ -20,6 +21,10 @@ class MainApplication(QMainWindow, Ui_MainApplication):
         self.bind_buttons()
         self.windows = []
         self.datacomm = DataComm()
+        self.tabel_model = TableModel(
+            self, self.datacomm.data, self.datacomm.header
+        )
+        self.update_table()
 
     def init_UI(self):
         """
@@ -40,7 +45,7 @@ class MainApplication(QMainWindow, Ui_MainApplication):
 
     def bind_buttons(self):
         self.create_task_button.clicked.connect(self.add_task)
-        # Added Edit task button
+        self.remove_task_button.clicked.connect(self.remove_task)
         self.edit_task_button.clicked.connect(self.edit_task)
 
     def add_task(self):
@@ -50,9 +55,16 @@ class MainApplication(QMainWindow, Ui_MainApplication):
         form = AddTask(self.datacomm)
         self.windows.append(form)
 
+    def remove_task(self):
+        self.datacomm.delete_task()
+
     def edit_task(self):
         pass
         #edit_task_button.setText("Press Me")
+
+    def update_table(self):
+        self.task_table.setModel(self.tabel_model)
+        self.task_table.setSortingEnabled(True)
 
     def closeEvent(self, event):
         """
