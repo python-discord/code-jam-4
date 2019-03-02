5312d46dc57a08634d3c3ca491c157c2bdca3281
ravenkls <kristian.smith.2001@hotmail.co.uk>
2019-03-02 11:24:23 +0000

    added support for high dpi displays

--- project/__main__.py
+++ project/__main__.py
@@ -1,9 +1,13 @@
 from PyQt5.QtWidgets import QApplication
+from PyQt5.QtCore import Qt
 from .gui import MinesweeperApp
 from . import resources  # noqa
 import sys
 
 if __name__ == '__main__':
+    # For high DPI displays
+    QApplication.setAttribute(Qt.AA_EnableHighDpiScaling, True)
+
     app = QApplication(sys.argv)
 
     window = MinesweeperApp()
