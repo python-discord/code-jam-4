f39363664534411eb46b7e4c90a00bc787fb4c55
Duck <chris13888@gmail.com>
2019-03-02 13:31:10 -0800

    Changed password to passphrase. Added comments.

--- project/ui/password_prompt.py
+++ project/ui/password_prompt.py
@@ -3,7 +3,7 @@
 # Form implementation generated from reading ui file 'qt/passwordprompt.ui',
 # licensing of 'qt/passwordprompt.ui' applies.
 #
-# Created: Thu Feb 28 21:34:32 2019
+# Created: Sat Mar  2 13:02:35 2019
 #      by: pyside2-uic  running on PySide2 5.12.1
 #
 # WARNING! All changes made in this file will be lost!
@@ -15,23 +15,23 @@ class Ui_PasswordPrompt(object):
         PasswordPrompt.setObjectName("PasswordPrompt")
         PasswordPrompt.resize(400, 300)
         self.instructions = QtWidgets.QTextBrowser(PasswordPrompt)
-        self.instructions.setGeometry(QtCore.QRect(10, 10, 381, 41))
+        self.instructions.setGeometry(QtCore.QRect(10, 60, 381, 41))
         self.instructions.setObjectName("instructions")
         self.instructions_backwards = QtWidgets.QTextBrowser(PasswordPrompt)
-        self.instructions_backwards.setGeometry(QtCore.QRect(10, 120, 381, 41))
+        self.instructions_backwards.setGeometry(QtCore.QRect(10, 140, 381, 41))
         self.instructions_backwards.setObjectName("instructions_backwards")
         self.password_label = QtWidgets.QLabel(PasswordPrompt)
-        self.password_label.setGeometry(QtCore.QRect(10, 70, 71, 20))
+        self.password_label.setGeometry(QtCore.QRect(10, 110, 71, 20))
         self.password_label.setObjectName("password_label")
         self.password_regular = QtWidgets.QLineEdit(PasswordPrompt)
-        self.password_regular.setGeometry(QtCore.QRect(60, 70, 321, 20))
+        self.password_regular.setGeometry(QtCore.QRect(60, 110, 321, 20))
         self.password_regular.setEchoMode(QtWidgets.QLineEdit.Password)
         self.password_regular.setObjectName("password_regular")
         self.password_label_2 = QtWidgets.QLabel(PasswordPrompt)
-        self.password_label_2.setGeometry(QtCore.QRect(10, 180, 71, 20))
+        self.password_label_2.setGeometry(QtCore.QRect(10, 190, 71, 20))
         self.password_label_2.setObjectName("password_label_2")
         self.password_backwards = QtWidgets.QLineEdit(PasswordPrompt)
-        self.password_backwards.setGeometry(QtCore.QRect(60, 180, 321, 20))
+        self.password_backwards.setGeometry(QtCore.QRect(60, 190, 321, 20))
         self.password_backwards.setEchoMode(QtWidgets.QLineEdit.Password)
         self.password_backwards.setObjectName("password_backwards")
         self.confirm_button = QtWidgets.QPushButton(PasswordPrompt)
@@ -44,6 +44,10 @@ class Ui_PasswordPrompt(object):
         self.correct_label.setGeometry(QtCore.QRect(10, 230, 381, 20))
         self.correct_label.setText("")
         self.correct_label.setObjectName("correct_label")
+        self.generated_passphrase = QtWidgets.QLabel(PasswordPrompt)
+        self.generated_passphrase.setGeometry(QtCore.QRect(10, 20, 381, 20))
+        self.generated_passphrase.setText("")
+        self.generated_passphrase.setObjectName("generated_passphrase")
 
         self.retranslateUi(PasswordPrompt)
         QtCore.QMetaObject.connectSlotsByName(PasswordPrompt)
@@ -54,12 +58,12 @@ class Ui_PasswordPrompt(object):
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
 "p, li { white-space: pre-wrap; }\n"
 "</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8.25pt; font-weight:400; font-style:normal;\">\n"
-"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Please enter your password as a security precaution.</p></body></html>", None, -1))
+"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Please reorder the passphrase into alphabetical order.</p></body></html>", None, -1))
         self.instructions_backwards.setHtml(QtWidgets.QApplication.translate("PasswordPrompt", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
 "p, li { white-space: pre-wrap; }\n"
 "</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8.25pt; font-weight:400; font-style:normal;\">\n"
-"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Enter your password again but backwards.</p></body></html>", None, -1))
+"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Enter the alphabetically foremost word of the passphrase, but backwards.</p></body></html>", None, -1))
         self.password_label.setText(QtWidgets.QApplication.translate("PasswordPrompt", "Password", None, -1))
         self.password_label_2.setText(QtWidgets.QApplication.translate("PasswordPrompt", "Password", None, -1))
         self.confirm_button.setText(QtWidgets.QApplication.translate("PasswordPrompt", "Confirm", None, -1))
--- project/widgets/password_prompt.py
+++ project/widgets/password_prompt.py
@@ -1,8 +1,30 @@
 from PySide2.QtWidgets import QWidget
+from random import choice
 
 from project import ui
 
-PASSWORD = "rainbow"
+words = """
+salty
+fish
+lackadaisical
+lax
+llama
+lukewarm
+leisurely
+enthusiastic
+fantastic
+flying
+alphabetical
+gamma
+terrier
+excitable
+green
+purple
+superb
+stressed
+machine
+hunter
+""".split('\n')[1:-1]
 
 
 class PasswordPrompt(QWidget):
@@ -14,13 +36,25 @@ class PasswordPrompt(QWidget):
 
         self.ui.cancel_button.pressed.connect(self.close)
         self.ui.confirm_button.pressed.connect(self.check)
+        self.success = -1
+
+    def display(self):
+        self.success = -1
+        self.passphrase = [choice(words), choice(words), choice(words)]
+        self.ui.generated_passphrase.setText(
+            f"Your generated passphrase is <b>{' '.join(self.passphrase)}</b>."
+        )
+        self.show()
 
     def check(self):
+        """Check if entered passphrase is correct."""
         field_one = self.ui.password_regular.text()
         field_two = self.ui.password_backwards.text()
-        if (field_one == PASSWORD) and (field_two == PASSWORD[::-1]):
+        if (field_one == ' '.join(sorted(self.passphrase))) and \
+           (field_two == sorted(self.passphrase)[0][::-1]):
+            self.success = 1
             self.close()
-            return 1
         else:
-            self.ui.correct_label.setText("Password incorrect. Try again.")
-            return 0
+            self.ui.correct_label.setText(
+                "<span style='color:red'>Passphrase incorrect. Try again.</span>"
+            )
