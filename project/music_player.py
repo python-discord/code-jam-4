e5894d6b3fa64f4d17bd9fd4c4e3abfeb70252a0
Duck <chris13888@gmail.com>
2019-03-02 14:22:43 -0800

    Moved location of create_db function

--- project/music_player.py
+++ project/music_player.py
@@ -6,6 +6,35 @@ from project import playlist
 from project.widgets import MainWindow
 
 
+def create_db():
+    """Create the playlist's database file and table if they don't exist.
+
+    The created database should be accessed using :meth:`PySide2.QtSql.QSqlDatabase.database`, the
+    name being specified via :const:`project.library.DB_NAME`.
+
+    """
+    with sqlite3.connect(DB_NAME) as conn:
+        cursor = conn.cursor()
+        cursor.execute("""
+            create table if not exists playlist (
+                id     integer primary key,
+                title  text,
+                artist text,
+                album  text,
+                genre  text,
+                date   text,
+                crc32  integer not null,
+                path   text    not null
+            );
+        """)
+        conn.commit()
+
+    db = QSqlDatabase.addDatabase("QSQLITE")
+    db.setDatabaseName(DB_NAME)
+
+    # TODO: Handle possible errors if db fails to open
+
+
 def main():
     app = QApplication(sys.argv)
     app.setApplicationName("Music Player")
--- project/playlist.py
+++ project/playlist.py
@@ -233,32 +233,3 @@ class Playlist(QMediaPlaylist):
 
     def save(self, *args, **kwargs):
         raise NotImplementedError
-
-
-def create_db():
-    """Create the playlist's database file and table if they don't exist.
-
-    The created database should be accessed using :meth:`PySide2.QtSql.QSqlDatabase.database`, the
-    name being specified via :const:`project.library.DB_NAME`.
-
-    """
-    with sqlite3.connect(DB_NAME) as conn:
-        cursor = conn.cursor()
-        cursor.execute("""
-            create table if not exists playlist (
-                id     integer primary key,
-                title  text,
-                artist text,
-                album  text,
-                genre  text,
-                date   text,
-                crc32  integer not null,
-                path   text    not null
-            );
-        """)
-        conn.commit()
-
-    db = QSqlDatabase.addDatabase("QSQLITE")
-    db.setDatabaseName(DB_NAME)
-
-    # TODO: Handle possible errors if db fails to open
