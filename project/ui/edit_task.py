b05e8b6d2e99db7146784a15f7f2b79bdcf56141
kosayoda <kieransiek@gmail.com>
2019-03-03 21:34:25 +0800

    Removed unused imports, linted and removed need of global import

--- project/ui/edit_task.py
+++ project/ui/edit_task.py
@@ -1,34 +1,39 @@
-import sys
 import random
-from PySide2 import QtCore, QtWidgets, QtGui
+from PySide2 import QtCore, QtWidgets
+
 
 class MyWidget(QtWidgets.QWidget):
-	global count
-	count = 0
-	
-	def __init__(self):
-		QtWidgets.QWidget.__init__(self)
-		
-		self.greetings = ["Are you sure you wanna edit?", "It is weird that you want to edit.", "Why do you want to edit", "I do not think that is a wise choice", "Beep! Beep! Wrong choice!"]
-		
-		self.button = QtWidgets.QPushButton("Click to edit")
-		self.text = QtWidgets.QLabel("Edit Task?")
-		self.text.setAlignment(QtCore.Qt.AlignCenter)
+    count = 0
 
-		self.layout = QtWidgets.QVBoxLayout()
-		self.layout.addWidget(self.text)
-		self.layout.addWidget(self.button)
-		self.setLayout(self.layout)
-	
-		self.button.clicked.connect(self.magic)
-		
-	def magic(self):
-		global count
+    def __init__(self):
+        QtWidgets.QWidget.__init__(self)
+        self.setWindowTitle("Edit Task")
+        self.resize(300, 200)
 
-		if count < 5:
-			self.text.setText(random.choice(self.greetings))
-			count += 1
-		else:
-			self.text = QtWidgets.QLabel("Sorry! Wrong Answer! Function is disabled..")
-			self.button.setEnabled(False)
-			self.button.setDisabled(True)
+        self.greetings = [
+            "Are you sure you wanna edit?",
+            "It is weird that you want to edit.",
+            "Why do you want to edit?",
+            "I do not think that is a wise choice",
+            "Beep! Beep! Wrong choice!"
+        ]
+
+        self.button = QtWidgets.QPushButton("Click to edit")
+        self.text = QtWidgets.QLabel("Edit Task?")
+        self.text.setAlignment(QtCore.Qt.AlignCenter)
+
+        self.layout = QtWidgets.QVBoxLayout()
+        self.layout.addWidget(self.text)
+        self.layout.addWidget(self.button)
+        self.setLayout(self.layout)
+
+        self.button.clicked.connect(self.magic)
+
+    def magic(self):
+        if MyWidget.count < 5:
+            self.text.setText(random.choice(self.greetings))
+            MyWidget.count += 1
+        else:
+            self.text = QtWidgets.QLabel("Sorry! Wrong Answer! Function is disabled..")
+            self.button.setEnabled(False)
+            self.button.setDisabled(True)
