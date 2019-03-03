0200179e899f513bf8849a9b122643f3850a09b9
remytanx <remy.tan@gmail.com>
2019-03-03 16:28:08 +0800

    Remove unnecessary statements

--- project/ui/edit_task.py
+++ project/ui/edit_task.py
@@ -5,11 +5,9 @@ from PySide2 import QtCore, QtWidgets, QtGui
 class MyWidget(QtWidgets.QWidget):
 	global count
 	count = 0
-	print("count:", count)
+	
 	def __init__(self):
 		QtWidgets.QWidget.__init__(self)
-		
-		# self.hello = ["Hallo Welt", "你好，世界", "Hei maailma", "Hola Mundo", "Привет мир"]
 		
 		self.greetings = ["Are you sure you wanna edit?", "It is weird that you want to edit.", "Why do you want to edit", "I do not think that is a wise choice", "Beep! Beep! Wrong choice!"]
 		
@@ -26,7 +24,7 @@ class MyWidget(QtWidgets.QWidget):
 		
 	def magic(self):
 		global count
-		print("count:", count)
+
 		if count < 5:
 			self.text.setText(random.choice(self.greetings))
 			count += 1
@@ -34,15 +32,3 @@ class MyWidget(QtWidgets.QWidget):
 			self.text = QtWidgets.QLabel("Sorry! Wrong Answer! Function is disabled..")
 			self.button.setEnabled(False)
 			self.button.setDisabled(True)
-			return True
-		print("count:", count)
-		
-			
-
-# if __name__ == "__main__":
-	# app = QtWidgets.QApplication(sys.argv)
-	
-	# widget = MyWidget()
-	# widget.show()
-	
-	# sys.exit(app.exec_())
