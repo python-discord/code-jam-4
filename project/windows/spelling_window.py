3dc25aeeaf2fb8c22ed093b2370fece591940981
Hanyuan Li <Qwerp-Derp@users.noreply.github.com>
2019-03-03 21:42:15 +1100

    Added a list of misspellings

--- project/windows/spelling_window.py
+++ project/windows/spelling_window.py
@@ -1,6 +1,30 @@
 import tkinter as tk
 
+from project.spelling import *
+
+
 class SpellingWindow(tk.Toplevel):
     def __init__(self, master, word):
         super().__init__(master)
         self.word = word
+
+    def can_misspell(self):
+        # If the word is correct already, we can't correct
+        # it further
+        if is_correct(self.word):
+            return False
+
+        # If a word can't be misspelled, when we call `misspell()`
+        # on it, the result will be the same as the word.
+        return self.word != misspell(self.word)
+
+    def misspellings(self):
+        result = []
+
+        while len(result) < 4:
+            misspelling = misspell(self.word)
+
+            if misspelling not in result:
+                result.append(misspelling)
+
+        return result
