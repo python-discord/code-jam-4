b2e46bb59e0883ff96eb66bab2374c4ec04227a9
zomatree <39768508+zomatree@users.noreply.github.com>
2019-03-03 21:26:03 +0000

    Zomatree (#20)
    * Update application.py
    
    added more indepth date checking and addedback button :smile:
    
    * fixed bug that alows you to have month be 0
    
    * fixed linting and bug where month/day could be 0
    
    * Update application.py
    
    changd inputs to tk.Text from tk.Entry and resized the entry boxes also when you create a event is updates the list
    
    * Update application.py
    
    added the start of horrible looks and the hatrid
    
    * updated __main__ and app.py
    
    this one is anoying
    all black,
    text is near black
    to click you have to hold left click when clicking a button a popup will apear press enter then let go of left click on the let go the button will click
    also closing the app may be hard
    
    * added more hatrid
    
    * removed random import
    
    * fixed linting issue
    
    * fix for no lowercase in entrys
    
    * half finsihed but have to go try finished this

--- project/__main__.py
+++ project/__main__.py
@@ -1,5 +1,18 @@
 from .UI.application import Application
+from tkinter import messagebox as mb
+
+
+def protocal():
+    if mb.askyesno(None, "Are you sure you want to quit?"):
+        quit()
+    else:
+        pass
+
 
 if __name__ == "__main__":
     app = Application()
+    # app.bind("<Button-1>", lambda event: mb.showinfo(
+    #     "UPDATE!",
+    #     "we have updated our privacy policy"))
+    app.protocol("WM_DELETE_WINDOW", protocal)
     app.mainloop()
