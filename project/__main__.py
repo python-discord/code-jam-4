1487852feffc820917ae0646c877e8607cae0cfd
HBenches <32874363+HBenches@users.noreply.github.com>
2019-03-03 16:57:53 +0000

    adds bgm. somewhat of a hacky solution (see comment)

--- project/__main__.py
+++ project/__main__.py
@@ -1,5 +1,6 @@
 from PyQt5.QtWidgets import QApplication
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QUrl
+from PyQt5 import QtMultimedia
 from .gui import MinesweeperApp
 from . import resources  # noqa
 import sys
@@ -9,6 +10,14 @@ if __name__ == '__main__':
     QApplication.setAttribute(Qt.AA_EnableHighDpiScaling, True)
 
     app = QApplication(sys.argv)
+
+    bgmplaylist = QtMultimedia.QMediaPlaylist()
+    bgmplaylist.addMedia(QtMultimedia.QMediaContent(
+        QUrl.fromLocalFile("./project/resources/elevator.mp3")))
+    bgmplaylist.setPlaybackMode(QtMultimedia.QMediaPlaylist.Loop)
+    bgmplayer = QtMultimedia.QMediaPlayer()
+    bgmplayer.setPlaylist(bgmplaylist)
+    bgmplayer.play()
 
     window = MinesweeperApp()
     window.show()
